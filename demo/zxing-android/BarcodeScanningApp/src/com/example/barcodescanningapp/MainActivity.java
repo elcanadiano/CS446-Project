8052c0192648f8ae8f52921fcb3851eac9cb52bb
y22yin <y22yin@uwaterloo.ca>
2014-02-20 16:55:54 -0500

    added buttons to main screen

--- demo/zxing-android/BarcodeScanningApp/src/com/example/barcodescanningapp/MainActivity.java
+++ demo/zxing-android/BarcodeScanningApp/src/com/example/barcodescanningapp/MainActivity.java
@@ -1,23 +1,8 @@
 package com.example.barcodescanningapp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.Charset;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
-
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -27,44 +12,71 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends Activity implements OnClickListener{
-	private Button scanBtn;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	//UI instance variables
+	private Button scanBtn, postBtn, searchBtn;
 	private TextView formatTxt, contentTxt;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		//instantiate UI items
 		scanBtn = (Button)findViewById(R.id.scan_button);
+		postBtn = (Button)findViewById(R.id.post_button);
+		searchBtn = (Button)findViewById(R.id.search_button);
 		formatTxt = (TextView)findViewById(R.id.scan_format);
 		contentTxt = (TextView)findViewById(R.id.scan_content);
-		
+
+		//listen for clicks
 		scanBtn.setOnClickListener(this);
+		postBtn.setOnClickListener(this);
+		searchBtn.setOnClickListener(this);
 	}
-	
-	public void onClick(View v) {
-		if (v.getId() == R.id.scan_button) {
-			IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-			scanIntegrator.initiateScan();
+
+	public void onClick(View v){
+		
+		
+
+		switch (v.getId()) {
+			case R.id.scan_button:
+				IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+				scanIntegrator.initiateScan();
+				break;
+			case R.id.post_button:
+				formatTxt.setText("FORMAT: "+"post");
+				contentTxt.setText("CONTENT: "+"button");
+				
+				break;
+			case R.id.search_button:
+				formatTxt.setText("FORMAT: "+"search");
+				contentTxt.setText("CONTENT: "+"button");
+				break;
 		}
 	}
-	
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-		
-		if (scanningResult != null) {
-			String scanContent = scanningResult.getContents();
-			String scanFormat = scanningResult.getFormatName();
-			
-			formatTxt.setText("FORMAT: "+scanFormat);
-			contentTxt.setText("CONTENT: "+ scanContent);
 
-		} else {
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		//retrieve result of scanning - instantiate ZXing object
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		//check we have a valid result
+		if (scanningResult != null) {
+			//get content from Intent Result
+			String scanContent = scanningResult.getContents();
+			//get format name of data scanned
+			String scanFormat = scanningResult.getFormatName();
+			//output to UI
+			formatTxt.setText("FORMAT: "+scanFormat);
+			contentTxt.setText("CONTENT: "+scanContent);
+		}
+		else{
+			//invalid scan data or scan canceled
 			Toast toast = Toast.makeText(getApplicationContext(), 
 					"No scan data received!", Toast.LENGTH_SHORT);
 			toast.show();
 		}
 	}
-	 
+
 }
