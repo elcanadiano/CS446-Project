205411e8b7b3f8d0e7a34ab724bc8bba7cf4de61
Carl <cecabusa@uwaterloo.ca>
2014-03-13 15:35:55 -0400

    added every colour pallte from flatui, added margins to listview, yaaaa

--- demo/zxing-android/BarcodeScanningApp/gen/com/example/barcodescanningapp/R.java
+++ demo/zxing-android/BarcodeScanningApp/gen/com/example/barcodescanningapp/R.java
@@ -15,10 +15,29 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        /**  Colour for Searching 
+        public static final int alizarin=0x7f07000f;
+        public static final int amethyst=0x7f070008;
+        public static final int asbestos=0x7f07000d;
+        public static final int belizehole=0x7f070007;
+        public static final int carrot=0x7f070011;
+        public static final int clouds=0x7f070002;
+        public static final int concrete=0x7f07000c;
+        public static final int emerald=0x7f070004;
+        public static final int greensea=0x7f070003;
+        public static final int midnightblue=0x7f07000b;
+        public static final int nephritis=0x7f070005;
+        public static final int orange=0x7f070014;
+        public static final int peterriver=0x7f070006;
+        public static final int pomegranate=0x7f070010;
+        public static final int pumpkin=0x7f070012;
+        public static final int silver=0x7f07000e;
+        public static final int sunflower=0x7f070013;
+        /** FLATUI Colour palette 
          */
         public static final int turqoise=0x7f070000;
+        public static final int wetasphalt=0x7f07000a;
         public static final int white=0x7f070001;
+        public static final int wisteria=0x7f070009;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -69,8 +88,8 @@ public final class R {
         public static final int search_scan=0x7f0a0016;
         public static final int search_text=0x7f0a0017;
         public static final int spinnerCourse=0x7f0a0007;
-        public static final int spinnerCourseNum=0x7f0a0008;
         public static final int spinnerTerm=0x7f0a0009;
+        public static final int textCourseNum=0x7f0a0008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
